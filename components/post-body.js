87918faf5dee6bfcaeb2512ef647f591d5def82a
Damjan Radev <radevdamjan@gmail.com>
2022-06-01 21:40:37 +0200

    fix

--- components/post-body.js
+++ components/post-body.js
@@ -1,11 +1,11 @@
-import postStyles from './post-styles.module.css'
+
 
 export default function PostBody({ content }) {
   console.log(content)
   return (
     
     <div
-      className={`post-content max-w-7xl mx-auto post ${postStyles.post}`}
+      className={`post-content max-w-7xl mx-auto post `}
       dangerouslySetInnerHTML={{ __html: content.html }}
     />
   )
