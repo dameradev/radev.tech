ca6d9eb3c2d531d8910ddd528cf46d941b6dac1b
Damjan Radev <radevdamjan@gmail.com>
2022-06-27 18:40:42 +0200

    New menu for mobile

--- components/header.js
+++ components/header.js
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 import ThemeSwitch from './ThemeSwitch'
 import Image from "next/image"
-import { MenuIcon } from "@heroicons/react/outline"
+import { MenuIcon, XCircleIcon } from "@heroicons/react/outline"
 import { useState } from 'react';
 
 export default function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false);
-  
+
 
   return (
     <header className=" bg-skin-header  shadow-md fixed w-full z-10 inset-0 h-fit  md:px-28">
@@ -45,10 +45,18 @@ export default function Header() {
         </nav>
 
         <div className="sm:hidden">
-          <MenuIcon className="w-8 cursor-pointer text-skin-secondary " onClick={() => setIsNavOpen(!isNavOpen)} />
-          <div className={`mt-10 w-full h-[100vh] absolute bg-skin-base z-10 top-10 left-0 transition-all ${isNavOpen ? 'translate-x-0': 'translate-x-[100%]' } `}>
-            <nav className='flex  flex-col items-center gap-8 relative top-0 mt-10 '>
-              <ul className="flex flex-col items-center gap-6 text-lg justify-center uppercase">
+          <div className="cursor-pointer text-skin-secondary bg-slate-700 p-2 rounded-full hover:opacity-90 border-2 border-transparent ease-in hover:border-secondary">
+            <MenuIcon className="w-6" onClick={() => setIsNavOpen(!isNavOpen)} />
+          </div>
+          {/* {isNavOpen && <div className='w-[100vw] h-[100vh] absolute bg-white top-0 left-0 opacity-30' />} */}
+          {isNavOpen && 
+            <div className='fixed inset-0 bg-black/20 backdrop-blur-sm dark:bg-gray-900/80 ' onClick={() => setIsNavOpen(!isNavOpen)} />
+          }
+           <div className={`mt-10 w-fit h-fit  absolute bg-skin-base z-10 top-[-12px] right-5 transition-all p-5 px-5 rounded-md ${isNavOpen ? 'translate-x-0' : 'translate-x-[120%]'} `}>
+
+            <nav className='flex  items-center gap-8 relative top-0  '>
+
+              <ul className="flex flex-col  gap-6 text-lg justify-center uppercase mr-15">
                 <li className="">
                   <Link href="/">
                     <a>Home</a>
@@ -65,11 +73,13 @@ export default function Header() {
                   </Link>
                 </li>
               </ul>
-              <ThemeSwitch />
+              <XCircleIcon className='w-8 self-start cursor-pointer' onClick={() => setIsNavOpen(false)} />
             </nav>
+            <div className="flex justify-center w-full mt-6">
+              <ThemeSwitch />
+            </div>
           </div>
         </div>
-
       </div>
 
     </header>
