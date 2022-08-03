a7b4ab99e013723a1896d692ded8b3c49780b14e
Damjan Radev <radevdamjan@gmail.com>
2022-08-03 15:57:37 +0200

    Generated sitemap file

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+const config = {
+  siteUrl: process.env.SITE_URL || 'https://radev.tech',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
+
+module.exports = config
--- next-sitemap.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://blog-graphcms-weld.vercel.app",
-  generateRobotsTxt: true, // (optional)
-};
