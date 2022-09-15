53362e8fa7950126389aaa1e52baf0875c3d2889
Damjan Radev <radevdamjan@gmail.com>
2022-09-15 11:02:33 +0200

    Redesigned footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,59 +8,61 @@ export default function Footer() {
   return (
     <footer className="border-t border-accent-2 bg-base text-text mt-10">
       <Container className="">
-        <div className="pt-14 pb-10 flex flex-col lg:flex-row">
-          <h2 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2 italic">
+
+        <div className=" w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center justify-items-center py-6 z-20">
+
+          <nav className='hidden md:flex items-center gap-8 justify-self-start'>
+            <ul className="flex flex-col gap-4 justify-center uppercase">
+              <li className="mr-4 ">
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li className="mr-4">
+                <Link href="/about">
+                  <a>About</a>
+                </Link>
+              </li>
+              <li className="mr-4">
+                <Link href="/contact">
+                  <a>Contact</a>
+                </Link>
+              </li>
+            </ul>
+
+            <ThemeSwitch />
+          </nav>
+
+          <h2 className="text-5xl font-bold min-w-full justify-self-center text-center">
             Damian Radev
           </h2>
 
           {/* Social media links with icons display in an ul list */}
-          <div className="flex justify-between w-full">
+          <div className='mt-6 md:mt-0 md:justify-self-end'>
+            <p className="text-xl">Connect with me</p>
+            <ul className="mt-4 flex flex-col justify-end items-center md:items-end gap-4">
+              <li >
+                <a
+                  href="https://twitter.com/dameradev"
 
-            <div>
-              <p className="text-xl">Connect with me</p>
-              <ul className="mt-4 flex  gap-4">
-                <li >
-                  <a
-                    href="https://twitter.com/dameradev"
 
-                  
-                  >
-                    <FaTwitter className="w-8 h-8" />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/dameradev"
+                >
+                  <FaTwitter className="w-8 h-8" />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/dameradev"
 
-                    
-                  >
 
-                    <FaGithub className="w-8 h-8" />
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <nav className='hidden md:flex items-center gap-8'>
-              <ul className="flex flex-col gap-4 justify-center uppercase">
-                <li className="mr-4 ">
-                  <Link href="/">
-                    <a>Home</a>
-                  </Link>
-                </li>
-                <li className="mr-4">
-                  <Link href="/about">
-                    <a>About</a>
-                  </Link>
-                </li>
-                <li className="mr-4">
-                  <Link href="/contact">
-                    <a>Contact</a>
-                  </Link>
-                </li>
-              </ul>
-              
-              <ThemeSwitch />
-            </nav>
+                >
+
+                  <FaGithub className="w-8 h-8" />
+                </a>
+              </li>
+            </ul>
+
+
           </div>
 
 
---
+++ pages/projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const Projects = ({ preview }) => {
+  return (
+    <Layout preview={preview}>
+      <h1>Projects</h1> 
+    </Layout>
+  );
+};
+
+export default Projects;
