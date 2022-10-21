9c5734505478d3c092c8a6cb10bc1ef0e2966ef8
Damjan Radev <radevdamjan@gmail.com>
2022-10-21 14:32:01 +0200

    Ux improvement for more stories

--- components/PostPreview.tsx
+++ components/PostPreview.tsx
@@ -4,10 +4,24 @@ import CoverImage from './CoverImage'
 import Link from 'next/link'
 import styled from 'styled-components'
 import TimeToRead from './TimeToRead'
+import { device } from '../styles/deviceSIzes'
 // import { getFeaturedImage } from '../lib/wordpress'
 
 
 const PostStyled = styled.div`
+
+  border-bottom: 1px solid var(--accent-3); 
+  &:first-of-type {
+    border-top: 1px solid #747474; 
+    padding-top: 3rem;
+  }
+  
+  &:last-of-type { 
+    
+    border-bottom: none; 
+  }
+
+  padding-bottom: 2rem;
   &:hover { 
     img {
       opacity: 0.8;
@@ -21,6 +35,13 @@ const PostStyled = styled.div`
     -webkit-box-orient: vertical;  
     overflow: hidden;
   }
+
+  @media ${device.tabletMini} {
+    flex-direction: column-reverse;
+    display: flex;
+  }
+
+
 `
 
 export default function PostPreview({
@@ -34,26 +55,30 @@ export default function PostPreview({
 
   return (
     <PostStyled>
-      
-      <div className="mb-5 relative">
-         {coverImage && <CoverImage slug={slug} title={title.rendered} url={coverImage} />}
-      </div>
-      
+
+      <Link href={`/posts/${slug}`}>
+        <a className="mb-5 block relative">
+          {coverImage && <CoverImage slug={slug} title={title.rendered} url={coverImage} />}
+        </a>
+      </Link>
       <div className="mb-4 text-xs uppercase flex gap-x-4 items-center">
         <Date dateString={post.publishDate} />
         <TimeToRead timeToRead={timeToRead} />
       </div>
-      <h3 className="mb-6 text-3xl leading-snug">
-        <Link href={`/posts/${slug}`}>
-          <a className="hover:underline">{title}</a>
-        </Link>
-      </h3>
+      <div className='flex flex-col'>
 
-      <div className="excerpt mb-4 text-lg leading-relaxed"
-        dangerouslySetInnerHTML={{
-          __html: excerpt
-        }}>
+        <h3 className="mb-6 text-3xl leading-snug">
+          <Link href={`/posts/${slug}`}>
+            <a className="hover:underline">{title}</a>
+          </Link>
+        </h3>
 
+        <div className="excerpt mb-4 text-lg leading-relaxed"
+          dangerouslySetInnerHTML={{
+            __html: excerpt
+          }}>
+
+        </div>
       </div>
 
     </PostStyled>
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,17 @@ const themes = {
     gray: "var(--color-gray)",//withOpacity("--color-header"),
     'info-window': "var(--color-info-window)",//withOpacity("--color-header"),
     'code': "var(--color-code)",//withOpacity("--color-header"),
+
+    'accent-0': 'var(--accent-0)',
+    'accent-1': 'var(--accent-1)',
+    'accent-2': 'var(--accent-2)',
+    'accent-3': 'var(--accent-3)',
+    'accent-4': 'var(--accent-4)',
+    'accent-5': 'var(--accent-5)',
+    'accent-6': 'var(--accent-6)',
+    'accent-7': 'var(--accent-7)',
+    'accent-8': 'var(--accent-8)',
+    'accent-9': 'var(--accent-9)',
   },
 }
 
