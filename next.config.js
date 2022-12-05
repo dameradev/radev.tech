de7f4f12b957c8c04417b5301948b498fa6bab9d
Damjan Radev <radevdamjan@gmail.com>
2022-12-05 15:49:31 +0100

    Another image provider

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   swcMinify: true,
   
   images: {
-    domains: ["media.graphassets.com", "www.tum.thz.mybluehost.me", "s3.us-west-2.amazonaws.com", "res.cloudinary.com", "scontent.cdninstagram.com", "scontent-iad3-3.cdninstagram.com", "scontent-iad3-1.cdninstagram.com", "scontent-iad3-2.cdninstagram.com"],
+    domains: ["media.graphassets.com", "www.tum.thz.mybluehost.me", "s3.us-west-2.amazonaws.com", "res.cloudinary.com", "scontent.cdninstagram.com", "scontent-iad3-3.cdninstagram.com", "scontent-iad3-1.cdninstagram.com", "fna.fbcdn.net"],
   },
   compiler: {
     // ssr and displayName are configured by default
