a076cd3a2261ecd2015847ee6f70376f3db3e85f
Damjan Radev <radevdamjan@gmail.com>
2022-12-05 13:37:41 +0100

    Added cdn instagram as an image provider

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   swcMinify: true,
   
   images: {
-    domains: ["media.graphassets.com", "www.tum.thz.mybluehost.me", "s3.us-west-2.amazonaws.com", "res.cloudinary.com", "scontent.cdninstagram.com"],
+    domains: ["media.graphassets.com", "www.tum.thz.mybluehost.me", "s3.us-west-2.amazonaws.com", "res.cloudinary.com", "scontent.cdninstagram.com", "cdninstagram.com"],
   },
   compiler: {
     // ssr and displayName are configured by default
